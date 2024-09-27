c3456f792070a2e8d5f7bb7741998bbb8268c4e6
Hurricane2206 <hurricane2206@proton.me>
2024-09-27 21:45:59 +0000

    changes

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 15;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "aurebesh:size=10" };
-static const char dmenufont[]       = "aurebesh:size=10";
+static const char *fonts[]          = { "aurebesh:size=14" };
+static const char dmenufont[]       = "aurebesh:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
